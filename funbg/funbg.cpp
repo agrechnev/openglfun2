eda51d7ddd5e0e5bde279388b4f64f46902cf5fa
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-10-02 16:38:03 +0300

    funbg

---
+++ funbg/funbg.cpp
@@ -0,0 +1,81 @@
+// By Oleksiy Grechnyev
+// FUN1: fun with OpenGL
+// Draw a triangle (or, optionally, rectangle) with MotokoGL library
+
+
+// System headers
+#include <iostream>
+#include <string>
+
+#include "MotokoGL.h"
+
+//=============================================================
+int main(){
+    using namespace MotokoGL;
+
+    Window window(800, 600, "Goblin OpenGL Fun Backgound");
+    
+    //-------------------------------------------
+    // Shaders
+    //-------------------------------------------
+    
+//    ShaderProg prog = ShaderProg::fromMem(vertexShaderSource, fragmentShaderSource);
+    ShaderProg prog = ShaderProg::fromFiles("shaders/vert.vert", "shaders/frag.frag");
+
+    //-------------------------------------------
+    // Model, VBO, VAO
+    //-------------------------------------------
+    // One triangle, w/o EBO
+    GLfloat verticesTr[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Square, with EBO
+    GLfloat verticesSq[] = {
+         0.5f,  0.5f, 0.0f,   1.0, 1.0,  // UR
+         0.5f, -0.5f, 0.0f,   1.0, 0.0,  // LR
+        -0.5f, -0.5f, 0.0f,   0.0, 0.0,  // LL
+        -0.5f,  0.5f, 0.0f,   0.0, 1.0   // UL
+    };
+    GLuint indicesSq[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // VAO for square (EBO)
+    Vao vao({3, 0, 2}, verticesSq, sizeof(verticesSq), indicesSq, sizeof(indicesSq));
+
+    // Textures
+    Tex tex = Tex::fromFile("common_textures/elves.jpg");
+
+    //-------------------------------------------
+    // GLFW game loop
+
+    while (!window.shouldClose() ){
+        glfwPollEvents();
+        
+        // Render
+        
+        // Clear screen
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // RGBA
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Draw our model
+        prog.use();
+
+        tex.bind(0);
+        glUniform1i(prog.loc("uTex"), 0);
+
+        vao.draw();
+
+        glBindVertexArray(0);
+        
+        // Draw the window (Swap buffers)
+        window.swapBuffers();
+    }
+
+    glfwTerminate(); // Stop openGL
+    return 0;
+}
